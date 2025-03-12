c9b19f2936b2876f247cf4424e9a4715de8900e5
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2025-03-12 23:11:45 +0100

    added a sql server setup file

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if 'created_at' not in kwargs.items() or 'updated_at' not in kwargs.items():
+        if 'created_at' not in kwargs.keys() or 'updated_at' not in kwargs.keys():
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
